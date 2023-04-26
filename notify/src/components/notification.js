376101b231863df8ffe6a936f4077fbe4cae78a7
tonyy007 <tonyyang1934@hotmail.com>
2023-04-26 00:29:12 -0500

    Timer works

--- notify/src/components/notification.js
+++ notify/src/components/notification.js
@@ -19,12 +19,38 @@ const NotificationForm = (props) => {
     setNotification(prevState => ({ ...prevState, [name]: value }));
   }
 
+  function delayUntilTime(targetTime, callback) {
+    
+    const currentTime = new Date();
+    const hour = Number(targetTime.slice(0,2));
+    const minute = Number(targetTime.slice(3,));
+    console.log(hour, minute);
+    var day = 0;
+    if(currentTime.getHours() > hour){
+      day = currentTime.getDate() + 1;
+    }else if(currentTime.getHours() < hour){
+      day = currentTime.getDate();
+    }else{ //equal
+      day = currentTime.getDate();
+    }
+    day = Number(day)
+    console.log(currentTime.getDate());
+    const desiredTime = new Date(2023,3,day,hour, minute,0);
+    const delay = desiredTime.getTime() - currentTime.getTime();
+    setTimeout(callback, delay);
+    console.log(delay, desiredTime.toLocaleString('en-US'), currentTime.toLocaleString('en-US'));
+  }
   const handleSubmit = (event) => {
     event.preventDefault();
-
-    const bellSound = new Audio('/bell.mp3');
-    bellSound.play();
+    delayUntilTime(notification.time, function() {
+      
     
+      const bellSound = new Audio('/bell.mp3');
+      bellSound.play();
+      const userSelectedColor = formData.color;
+      setBackgroundColor(userSelectedColor);
+      props.onBackgroundChange(userSelectedColor);
+    });
     /*
     setTimeout(() => {
       console.log('Finished!');
@@ -45,9 +71,7 @@ const NotificationForm = (props) => {
         formData[input.name] = input.value;
       }
     }
-    const userSelectedColor = formData.color;
-    setBackgroundColor(userSelectedColor);
-    props.onBackgroundChange(userSelectedColor);
+    
   
     // Format form data
     //const formDataDiv = document.createElement("div");
